c902f9f6433f62b8b65ff73d1acd2d9229efaf75
PatelPanth <146762440+PatelPanth@users.noreply.github.com>
2023-11-06 17:19:47 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,11 +39,11 @@ function draw() {
   
  //var select_sprites = Math(random(1,3));
 
- var select_sprites = Math.random(random(1,3));
+ //var select_sprites = Math.random(random(1,3));
 
 // var select_sprites = Math.round(1,3);
 
- //var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
   // if (frameCount % 50 == 0) {
@@ -57,12 +57,12 @@ function draw() {
   // }
 
    if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
+     if (select_sprites == 1) {
       createApples();
      } else if (select_sprites == 2) {
        createOrange();
      }
- //  }
+   }
 
   // if (frameCount / 80 == 0) {
   //   if (select_sprites == 1) {
@@ -75,8 +75,8 @@ function draw() {
   // }
 
   // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
+   //  if (select_sprites == 1) {
+   //    createApples();
   //   } else if (select_sprites == 2) {
   //     createOrange();
   //   }else {
